05e45b4f6d9e66031dc66d763ce3b4b422a1cb42
Shaikh Tasmiyah <tasmi.0609@gmail.com>
2026-09-24 00:01:41 +0530

    Load ML models from Hugging Face

--- backend/app.py
+++ backend/app.py
@@ -835,10 +835,10 @@ def upload_image():
         # -----------------------------------------------------
         # PEST MODEL
         # -----------------------------------------------------
-      pest_result = {
-    "detected": False,
-    "pests": [],
-    "confidence": None,
+         pest_result = {
+             "detected": False,
+             "pests": [],
+              "confidence": None,
 }
         image_url = f"/uploads/{filename}"
 
